6cd58b7c2f3e7c4509988aef827fa3ad37bade24
이나정 <leena9901@gmail.com>
2021-03-16 11:15:47 +0900

    add 10942

---
+++ 10942.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+void preset()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+}
+
+void init(){
+	
+}
+
+void solution(){
+
+}
+
+void output(){
+	
+}
+
+int main(){
+	preset();
+	init();
+	solution();
+	output();
+	return (0);
+}
