451bb89d4724d0b871fecd250904de135cb9adfc
Najeong Lee <najlee@c4r8s2.42seoul.kr>
2021-04-01 14:09:20 +0900

    modify 16926.py

--- 16926.py
+++ 16926.py
@@ -11,15 +11,15 @@ def Rotate_Square(i, N, M, arr):
 
 # arr 배열의 i, j 번째를 temp 배열에 돌려서 저장
 def move_point(i, j, start, saero, garo, arr, temp):
-    if (i == start and start < j <= garo - 1):
+    if (i == start and start < j <= garo - 1):        # 1
         temp[i][j-1] = arr[i][j]
-    elif (start <= i < saero - 1 and j == start): 
+    elif (start <= i < saero - 1 and j == start):     # 2
         temp[i+1][j] = arr[i][j]
-    elif (i == saero - 1 and start <= j < garo - 1):
+    elif (i == saero - 1 and start <= j < garo - 1):  # 3
         temp[i][j+1] = arr[i][j]
-    elif (start < i <= saero - 1 and j == garo - 1):
+    elif (start < i <= saero - 1 and j == garo - 1):  # 4
         temp[i-1][j] = arr[i][j]
-    else:
+    else:                                             # 5
         temp[i][j] = arr[i][j]
     return temp
 
@@ -34,10 +34,10 @@ N, M, R = map(int, input().split())
 arr =[[0 for _ in range(M)] for _ in range(N)]
 
 for i in range(N):
-    arr[i] = list(map(int, input().split()))
+    arr[i] = list(map(int, input().split())) # arr 입력받기
 
-square_num = min(N, M) // 2 #사각형의 개수
-rotate_num = R % 2*(N+M-2)
+square_num = min(N, M) // 2 # 사각형의 개수
+rotate_num = R % 2*(N+M-2)  # 회전하는 수 (4*4 사각형이면 2번 도는 것고 14번 도는 것이 같음!)
 for _ in range(R):
     for i in range(square_num):
         arr = Rotate_Square(i, N, M, arr)
