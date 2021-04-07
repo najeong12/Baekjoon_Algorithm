2bde6c80fe6f7c89fa6496493e4335b1f83adf0b
najeong12 <leena9901@gmail.com>
2021-04-07 18:13:31 +0900

    ad 11057.py

--- 11057.py
+++ 11057.py
@@ -6,7 +6,7 @@ dp[1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 for i in range(2, N+1):
     for j in range(10):
         if j == 0:
-            dp[i][j] = sum(dp[i-1]) % 10007
+            dp[i][j] = sum(dp[i-1])
         else:
             dp[i][j] = dp[i][j-1] - dp[i-1][j-1]
 
